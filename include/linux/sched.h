36d441b3a52c2f3957fa98a6714acb7c16eba47e
Uğurcan Polat <ugurcan.polat12@gmail.com>
2018-10-22 23:09:12 +0300

    Add custom flag to process descriptor

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1465,6 +1465,7 @@ struct task_struct {
 	unsigned int	sequential_io;
 	unsigned int	sequential_io_avg;
 #endif
+	unsigned int	myFlag;
 };
 
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
