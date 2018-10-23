21381d18a8891327513c29a9274a9dfbb29b916b
Uğurcan Polat <ugurcan.polat12@gmail.com>
2018-10-23 19:13:54 +0300

    Implement set_myFlag function

--- mycall/mycall.c
+++ mycall/mycall.c
@@ -3,5 +3,11 @@
 #include <linux/kernel.h>
 
 asmlinkage long set_myFlag(pid_t pid, int flag) {
+	struct task_struct *task;
+	
+	task = pid_task(find_vpid(pid), PIDTYPE_PID);
+	
+	task->myFlag = flag;
+	
 	return 0;
 }
