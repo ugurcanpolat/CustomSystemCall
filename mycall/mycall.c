c5686a7da7f5d32f1925f5e3a1f75ad97a3fe0e0
Uğurcan Polat <ugurcan.polat12@gmail.com>
2018-10-24 22:35:57 +0300

    Handle error cases in set_myFlag system call

--- mycall/mycall.c
+++ mycall/mycall.c
@@ -3,11 +3,26 @@
 #include <linux/kernel.h>
 
 asmlinkage long set_myFlag(pid_t pid, int flag) {
-	struct task_struct *task;
 	
+	/* Is value of flag valid? */
+	if (flag != 0 && flag != 1) 
+		return -EINVAL;
+	
+	/* Root access? */
+	if (!capable(CAP_SYS_ADMIN))
+		return -EPERM;
+	
+	struct task_struct *task;
 	task = pid_task(find_vpid(pid), PIDTYPE_PID);
 	
+	/* Unable to get find task */
+	if (task == NULL) 
+		return -ESRCH;
+		
+	/* Set flag */
 	task->myFlag = flag;
+	
+	printk("Flag has been set as %d for process with pid: %d\n", flag, pid);
 	
 	return 0;
 }
